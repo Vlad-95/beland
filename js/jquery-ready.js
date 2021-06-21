4e39e79823a775a4efd3a9779ebcee7d412f9425
vlad_z <vzv@siluetstudio.com>
2021-06-21 15:30:04 +0300

    logistics page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-    $('.slider').slick({
-        dots: false,
-        arrows: false
-    });
+    if($('.slider').length) {
+        $('.slider').slick({
+            dots: false,
+            arrows: false
+        });
+    }
 
     // цвет категорий на главной
     $('.category_main .category_main__item').each(function () {
@@ -14,14 +16,37 @@ $(document).ready(function() {
     })
 
     //Слайдер партнеров
-    $('.partners__slider').slick({
-        slidesToShow: 6,
-        dots: false,
-        arrows: true
-    });
+    if($('.partners__slider').length) {
+        $('.partners__slider').slick({
+            slidesToShow: 6,
+            dots: false,
+            arrows: true
+        });
+    }
 
     //одинаковая высота новостей
     if($('.news').length) {
         $('.news .news__item .news__item-content').matchHeight();
+    }
+
+    if(('#map').length) {
+        ymaps.ready(init);
+        function init(){
+            // Создание карты.
+            var myMap = new ymaps.Map("map", {
+                center: [59.85120307488966,30.30813975396727],
+                controls: ['geolocationControl'],                
+                zoom: 16
+            });
+
+            var myPlacemark = new ymaps.Placemark([59.85120307488966,30.30813975396727], {}, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/icons/map-marker.png',
+                iconImageSize: [55, 57],
+                iconImageOffset: [-28, -45]
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+        }
     }
 });
