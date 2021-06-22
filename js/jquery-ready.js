b213a65eb8347b6e1a17e8abacf7961dd9d7f2c3
Vladislav <vzv@siluetstudio.com>
2021-06-22 16:57:53 +0300

    filter for catalog category

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -7,13 +7,44 @@ $(document).ready(function() {
     }
 
     // цвет категорий на главной
-    $('.category_main .category_main__item').each(function () {
-        let color = $(this).data('color');
+    if($('.category_main').length) {
+        $('.category_main .category_main__item').each(function () {
+            let color = $(this).data('color');
+    
+            $(this).css('border-color', color);
+            $(this).find('.category_main__item-name').css('color', color);
+            $(this).find('.category_main__item-name path').css('fill', color);
+        });
+    }
+    
 
-        $(this).css('border-color', color);
-        $(this).find('.category_main__item-name').css('color', color);
-        $(this).find('.category_main__item-name path').css('fill', color);
-    })
+    // цвет категорий на странице 
+    if($('.catalog__menu').length) {
+        $('.catalog__menu .catalog__menu-item').each(function () {
+            let color = $(this).data('color');
+    
+            $(this).children('a').css('border-color', color);
+            
+        });
+    };
+
+    //аккордион категорий 
+    if($('.catalog__menu').length) {
+        $('.catalog__menu .catalog__menu-item .drop').click(function() {
+
+            $(this).closest('.catalog__menu-item').toggleClass('active');
+            $(this).siblings('.catalog__menu-sub').slideToggle();
+        })
+    }
+
+    //переключение цветов на странице категории
+    if($('.catalog__item-color .item').length) {
+        $('.catalog__item-color .item').click(function() {
+            let colorItemIndex = $(this).index();
+            $(this).addClass('active').siblings().removeClass('active');
+            $(this).closest('.catalog__item').find('.catalog__item-img').find('img').removeClass('active').eq(colorItemIndex).addClass('active')
+        })
+    }
 
     //Слайдер партнеров
     if($('.partners__slider').length) {
@@ -27,9 +58,9 @@ $(document).ready(function() {
     //одинаковая высота новостей
     if($('.news').length) {
         $('.news .news__item .news__item-content').matchHeight();
-    }
+    };
 
-    if(('#map').length) {
+    if($('#map').length) {
         ymaps.ready(init);
         function init(){
             // Создание карты.
