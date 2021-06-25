3fc9cc539a23605aa4c8c5a0aec14d9bfe52c00c
Vladislav <vzv@siluetstudio.com>
2021-06-25 16:52:21 +0300

    adaptive mainpage

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -202,7 +202,34 @@ $(document).ready(function() {
         $('.partners__slider').slick({
             slidesToShow: 6,
             dots: false,
-            arrows: true
+            arrows: true,
+            responsive: [
+                {
+                  breakpoint: 1200,
+                  settings: {
+                    slidesToShow: 5
+                    
+                  }
+                },
+                {
+                  breakpoint: 992,
+                  settings: {
+                    slidesToShow: 4
+                  }
+                },
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 3
+                  }
+                },
+                {
+                  breakpoint: 576,
+                  settings: {
+                    slidesToShow: 2
+                  }
+                }
+            ]
         });
     }
 
